4a548484ec1a27379cdb559ecbc363f0f2d57e8a
malverick <sumitlnmiit96@gmail.com>
2017-05-25 05:23:25 +0530

    Add documentation

--- plyer/facades/broadcast.py
+++ plyer/facades/broadcast.py
@@ -1,12 +1,45 @@
+'''
+Broadcast
+=========
+
+Broadcasts or In App Notifocations are notifications send by an app which are
+displayed when the user is active within the app itself.
+
+The :class:`Broadcast` provides access to public methods to send and receive
+in app notifications in your app itself.
+
+Simple Examples
+===============
+
+To send a broadcast message::
+
+    >>> from plyer import broadcast
+    >>> broadcast.send()
+
+To receive broadcast messsage::
+
+    >>> from plyer import broadcast
+    >>> broadcast.receive()
+
+'''
+
+
 class Broadcast(object):
     '''
     Broadcast facade.
     '''
 
     def send(self):
+        '''
+        Send broadcast to receiver.
+        '''
         return self._send()
 
     def recieve(self):
+        '''
+        Register a broadcast receiver to receive notifications from other
+        applications or the system itself.
+        '''
         return self._receive()
 
     #private
