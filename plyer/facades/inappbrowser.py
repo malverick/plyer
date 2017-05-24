1096670a72133f639b697108bc3e35ff965ecb41
malverick <sumitlnmiit96@gmail.com>
2017-05-25 00:08:08 +0530

    Typo fix

--- plyer/facades/inappbrowser.py
+++ plyer/facades/inappbrowser.py
@@ -1,30 +1,30 @@
 class InAppBrowser(object):
-	'''
-	In App Browser facade.
-	'''
+    '''
+    In App Browser facade.
+    '''
 
-	def load_page(self):
-		return _load_page()
+    def load_page(self):
+        return self._load_page()
 
-	def reload(self):
-		return _reload()
+    def reload(self):
+        return self._reload()
 
-	def go_back(self):
-		return _go_back()
+    def go_back(self):
+        return self._go_back()
 
-	def go_forward(self):
-		return _go_forward()
+    def go_forward(self):
+        return self._go_forward()
 
-	#private
+    #private
 
-	def _load_page(self):
-		raise NotImplementedError()
+    def _load_page(self):
+        raise NotImplementedError()
 
-	def _reload(self):
-		raise NotImplementedError()
+    def _reload(self):
+        raise NotImplementedError()
 
-	def _go_back(self):
-		raise NotImplementedError()
+    def _go_back(self):
+        raise NotImplementedError()
 
-	def _go_forward(self):
-		raise NotImplementedError()
+    def _go_forward(self):
+        raise NotImplementedError()
