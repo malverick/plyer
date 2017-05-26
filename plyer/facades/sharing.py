4cf47625690a871df415a4d7be73022f8e9ebe8c
malverick <sumitlnmiit96@gmail.com>
2017-05-26 21:49:20 +0530

    Add documentation

--- plyer/facades/sharing.py
+++ plyer/facades/sharing.py
@@ -1,12 +1,43 @@
+'''
+Sharing
+=======
+
+This API focuses on sharing data with different apps. For example, the built-in
+Browser app can share the URL of the currently-displayed page as text with any
+application. This is useful for sharing an article or website with friends via
+email or social networking.
+
+The :class:`Sharing` provides access to public methods to share data with
+different apps in your device.
+
+Simple Examples
+===============
+
+To share text::
+
+    >>> from plyer import sharing
+    >>> sharing.share(text)
+    # A window will open asking the user to choose the app with which he wants
+    to share the data.
+
+'''
+
+
 class Sharing(object):
-	'''
-	Sharing facade.
-	'''
+    '''
+    Sharing facade.
+    '''
 
-	def share(self, text):
-		return self._share()
+    def share(self, text):
+        '''
+        Share the text with different apps.
 
-	#private
+        :param text: Data to share
+        :type text: String
+        '''
+        return self._share()
 
-	def _share(self, text):
-		raise NotImplementedError()
+    #private
+
+    def _share(self, text):
+        raise NotImplementedError()
